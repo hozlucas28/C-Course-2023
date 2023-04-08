d7b1619a19c5760d29e802b2cc2631f2c30e488b
hozlucas28 <hozlucas28@gmail.com>
2023-04-08 00:54:00 -0300

    Remove conflicting lines

--- Pratices/Answers - 2023/Exercise - 01/library/library.h
+++ Pratices/Answers - 2023/Exercise - 01/library/library.h
@@ -1,11 +1,6 @@
-#ifndef LIBRARY_H
-#define LIBRARY_H
-
 #include <stdbool.h>
 #include <stdio.h>
 
 int getInputNaturalNumber();
 int getFactorial(const int *number);
 bool isNaturalNumber(const int *number);
-
-#endif
